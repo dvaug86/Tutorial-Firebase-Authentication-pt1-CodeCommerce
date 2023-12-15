ede95a6ef522bbfff480f7a4f0ecbc2025435b16
David Vaughan <dvaug86@gmail.com>
2023-12-15 13:04:49 -0500

    stylized components

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import Account from "./components/Account";
 
 function App() {
   return (
-    <div >
-     <h1 className="text-center text-3xl font-bold">
-      Firebase Auth & Context
-     </h1>
-     <Routes>
-      <Route path="/" element={<Signin/>} />
-      <Route path="/signup" element={<Signup/>} />
-      <Route path="/account" element={<Account/>} />
-     </Routes>
+    <div>
+      <h1 className="text-center text-3xl font-bold">
+        Firebase Auth & Context
+      </h1>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/account" element={<Account/>} />
+      </Routes>
     </div>
   );
 }
