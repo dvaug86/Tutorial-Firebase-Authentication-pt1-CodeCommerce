5ff20e957b3f0f6b37e6953ea2d7f29b3465d8c4
David Vaughan <dvaug86@gmail.com>
2023-12-15 09:32:21 -0500

    tied all pages to the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Account from "./components/Account";
 
 function App() {
   return (
-    <div className="App">
-     yo
+    <div >
+     <h1 className="text-center text-3xl font-bold">
+      Firebase Auth & Context
+     </h1>
+     <Routes>
+      <Route path="/" element={<Signin/>} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/account" element={<Account/>} />
+     </Routes>
     </div>
   );
 }
