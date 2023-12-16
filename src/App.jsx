542da59abbca0af124a569046d53351ec5b5cca5
David Vaughan <dvaug86@gmail.com>
2023-12-16 10:43:52 -0500

    added signup functions however there are bugs i Need to workout after I get home from work

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
 import Account from "./components/Account";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
@@ -9,11 +10,13 @@ function App() {
       <h1 className="text-center text-3xl font-bold">
         Firebase Auth & Context
       </h1>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/account" element={<Account/>} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
