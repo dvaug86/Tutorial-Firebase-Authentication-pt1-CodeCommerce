2f8241a90e693cc6dc3ab94aa65654ecfa7cbf3e
David Vaughan <dvaug86@gmail.com>
2024-02-02 09:03:33 -0500

    had to go to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { Route, Routes } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Account from "./components/Account";
 
 function App() {
   return (
@@ -5,6 +9,11 @@ function App() {
      <h1 className="text-center text-3xl font-bold">
       Firebase Auth & Context
      </h1>
+     <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signup" element={<Signup/>} />
+      <Route path="/account" element={<Account />} />
+     </Routes>
     </div>
   );
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="max-w-[700px] mx-auto my-16 p-4 ">
+      <div>
+        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
+        <p className="py-2">
+          Already have an account yet? <Link className="underline" to="/"> Sign in.</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
