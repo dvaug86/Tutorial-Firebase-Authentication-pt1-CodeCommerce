11e80b9616f7759f33fa63c8a8e83e842a03e57a
David Vaughan <dvaug86@gmail.com>
2024-02-02 08:04:55 -0500

    restart of the project

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ The tutorial is found here:
 https://www.youtube.com/watch?v=x62aBvnRCKw&t=2700s
 
 I had to take a break from working on this for the holidays and studing to get into a program. I am back but need to refresh on where I left off. Finished reviewing today. 
+
+I decided to start fresh on this project. The routes portion is going to be important so I really wanted to go through that again.
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { Route, Routes } from "react-router-dom";
-import Signin from "./components/Signin";
-import Signup from "./components/Signup";
-import Account from "./components/Account";
-import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <div>
-      <h1 className="text-center text-3xl font-bold">
-        Firebase Auth & Context
-      </h1>
-      <AuthContextProvider>
-        <Routes>
-          <Route path="/" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-      </AuthContextProvider>
+     
     </div>
   );
 }
