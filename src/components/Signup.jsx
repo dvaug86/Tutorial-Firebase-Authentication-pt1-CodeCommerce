303810da7fdca90bd4fcdaa10419ad62b459d0fd
David Vaughan <dvaug86@gmail.com>
2024-02-03 13:12:29 -0500

    signup addition

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,9 +7,24 @@ const Signup = () => {
       <div>
         <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
         <p className="py-2">
-          Already have an account yet? <Link className="underline" to="/"> Sign in.</Link>
+          Already have an account yet?{" "}
+          <Link className="underline" to="/">
+            {" "}
+            Sign in.
+          </Link>
         </p>
       </div>
+      <form>
+        <div>
+          <label>Email Address</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label>Password</label>
+          <input className="py-2 font-medium" type="password" />
+        </div>
+        <button>Sign Up</button>
+      </form>
     </div>
   );
 };
